5d466141241741c44fa54cbcc56395cc04b7f0d4
beyzakilickol <43250495+beyzakilickol@users.noreply.github.com>
2018-12-12 21:21:22 -0600

    heroku

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const app = express()
 var bodyParser = require('body-parser')
-const PORT = 3050
+
 const pgp= require('pg-promise')()
-const connectionString = "postgres://jawjhwwcfexcvc:d74265908d5a3a4a829c9e78a29f46e95218aea2fadc3f8a1fe251c2db1a95cb@ec2-184-72-239-186.compute-1.amazonaws.com:5432/d5hjaoad2i7fgo"
-const db = pgp(connectionString)
 const bcrypt = require('bcrypt');
 var cors = require('cors')
 const jwt = require('jsonwebtoken')
 app.use(cors())
 // parse application/json
 app.use(bodyParser.json())
+const dotEnv = require('dotenv').config()
+const PORT = process.env.PORT || 3050;
+// connection string which is used to specify the location of the database
+const connectionString = process.env.DB_CONN
+  const config = {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      port: process.env.DB_PORT,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: true
+  }
+  // creating a new database object which will allow us to interact with the database
+  const db = pgp(config)
 //-----------------to enable CORS-------
 app.use(function(req, res, next) {
   //
